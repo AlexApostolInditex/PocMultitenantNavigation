927dacef32efd53c5bdc89130871b6e7cc268334
Alex Apostol <alexapo@ext.inditex.com>
2024-03-06 10:37:38 +0100

    feat(CORESERVICES-628): MultitenantPocNavigation remove mainStoryBoard and replace it with MainVc for starting point

---
+++ MultitenantNavigationPoc/Controllers/Main/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  MultitenantNavigationPoc
+//
+//  Created by alexandru.apostol on 6/3/24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
