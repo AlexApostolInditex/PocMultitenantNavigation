0f7a2ab3ea2d9945b19a7ad474bf0ed3de1ad947
Alex Apostol <alexapo@ext.inditex.com>
2024-03-06 11:00:16 +0100

    feat(CORESERVICES-628): MultitenantPocNavigation add ui for mainModuleVC

--- MultitenantNavigationPoc/Controllers/Main/View/MainViewController.swift
+++ MultitenantNavigationPoc/Controllers/Main/View/MainViewController.swift
@@ -11,17 +11,51 @@ public class MainViewController: UIViewController {
 
     private let viewModel: MainModuleViewModelProtocol
 
-    init(viewModel: MainModuleViewModelProtocol) {
+    private lazy var checkoutButton: UIButton = {
+        let button = UIButton(frame: .zero)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Go To Checkout", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+
+    private lazy var loginButton: UIButton = {
+        let button = UIButton(frame: .zero)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Go To Login", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+
+    private lazy var mainStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [loginButton, checkoutButton])
+        view.distribution = .fill
+        view.alignment = .fill
+        view.axis = .horizontal
+        view.spacing = 20
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    public init(viewModel: MainModuleViewModelProtocol) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
     }
     
-    required init?(coder: NSCoder) {
+    public required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
 
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
+        setUpMainStackViewLayout()
+        view.backgroundColor = .white
+    }
+
+    private func setUpMainStackViewLayout() {
+        view.addSubview(mainStackView)
+        mainStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        mainStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 }
--- MultitenantNavigationPoc/SceneDelegate.swift
+++ MultitenantNavigationPoc/SceneDelegate.swift
@@ -18,7 +18,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let navController = UINavigationController(rootViewController: MainViewController())
+        let model = MainModuleViewModel(coordinatorOutput: {_ in })
+        let navController = UINavigationController(rootViewController: MainViewController(viewModel: model))
         window?.rootViewController = navController
         window?.makeKeyAndVisible()
     }
